bfcbdc45c1ac9b1021ea9e3f9d13fe218210b658
ahmed <ahmedissaten@gmail.com>
2024-02-09 20:01:55 +0200

    reviw class

---
+++ models/review.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module defining the Review class."""
+from base_model import BaseModel
+
+
+class Review(BaseModel):
+    """A class to represent reviews.
+    Class Attributes:
+        place_id (str): The ID of the place associated with the review.
+        user_id (str): The ID of the user who created the review.
+        text (str): The text content of the review.
+    """
+    place_id = ""
+    user_id = ""
+    text = ""
