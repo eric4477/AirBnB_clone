ad5c23122d225dd466d187b376d05ac96d08b98d
ahmed <ahmedissaten@gmail.com>
2024-02-09 18:50:45 +0200

    amenity class

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+""" Amenity module """
+from base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """ represent amenity
+    Public class attributes:
+        name: string - empty string
+    """
+
+    name = ""
