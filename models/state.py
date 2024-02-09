53ae3b542355cc4b9e853455c1d17278f45b2e65
ahmed <ahmedissaten@gmail.com>
2024-02-09 20:09:11 +0200

    update the documentations

--- models/state.py
+++ models/state.py
@@ -1,10 +1,12 @@
+#!/usr/bin/python3
 """ state module """
 from base_model import BaseModel
 
 
 class State(BaseModel):
-    """
+    """ A class to represent states
     Class Attributes:
-        name: string - empty string
-        """
+        name (str): state name
+    """
+
     name = ""
