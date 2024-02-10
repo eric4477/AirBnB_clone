ebf69a7df00a5c38d7f796fa67eb84fc1faddb41
ahmed <ahmedissaten@gmail.com>
2024-02-10 19:53:15 +0200

    pycheck

--- models/user.py
+++ models/user.py
@@ -4,13 +4,15 @@ module for the <User> class
 """
 from models.base_model import BaseModel
 
-class User( BaseModel):
+
+class User(BaseModel):
     """
     <User> class that inherits from
     <BaseModel> class and handle user
     information
     """
-    email= ""
+
+    email = ""
     password = ""
     first_name = ""
     last_name = ""
