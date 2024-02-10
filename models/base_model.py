beb789e68e92d44d47be48583638a36e480c1422
ahmed <ahmedissaten@gmail.com>
2024-02-10 15:30:00 +0200

    add: class discription

--- models/base_model.py
+++ models/base_model.py
@@ -9,7 +9,10 @@ import models
 
 
 class BaseModel:
+    """ repersent BaseModel class """
+
     def __init__(self, *args, **kwargs):  # updated T-5
+        """ Base class constructor """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
