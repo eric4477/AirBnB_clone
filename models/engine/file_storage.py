361f956ec5996314a611cad24554b37faf98c69e
ahmed <ahmedissaten@gmail.com>
2024-02-10 14:57:20 +0200

    fix: file not found exception

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -17,14 +17,15 @@ class FileStorage:
         <class name>.id (ex: to store a BaseModel object with id=12121212,
         the key will be BaseModel.12121212)
     """
-    __file_path = "file.json"
-    __objects = {}
+    __file_path = "file.json"   # class attribute
+    __objects = {}              # class attribute
 
-    def all(self):
+    def all(self):  # returns the dictionary of all objects
         return FileStorage.__objects
 
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id
+        """adds the object obj with the key <obj class name>.id to
+                the __objects dictionary.
         """
         class_name = obj.__class__.__name__  # get the class name from object
         key = '{}.{}'.format(class_name, obj.id)
@@ -48,14 +49,18 @@ class FileStorage:
             otherwise, do nothing. If the file doesn’t exist,
             no exception should be raised)
         """
-        if os.path.isfile(FileStorage.__file_path):  # check if file path exist
-            with open(FileStorage.__file_path, "r") as my_file:
-                objs = json.load(my_file)  # load a dictionary form a file
 
-                for values in objs.values():
-                    # get the class name
-                    cls_name = values["__class__"]
-                    cls = eval(cls_name)
-                    instance = cls(**values)
+        try:
+            if os.path.isfile(FileStorage.__file_path):  # if file exist
+                with open(FileStorage.__file_path, "r") as my_file:
+                    objs = json.load(my_file)  # load a dictionary form a file
+
+                    for values in objs.values():
+                        # get the class name
+                        cls_name = values["__class__"]
+                        cls = eval(cls_name)  # get the actual class
+                        instance = cls(**values)   # create instance
 
                     FileStorage.__objects['id'] = instance
+        except FileNotFoundError:  # if file note found do nothing
+            return
