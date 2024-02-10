d6fe4df6ef4e0e40cdba73409595cb2975e4ca8e
ahmed <ahmedissaten@gmail.com>
2024-02-10 23:11:55 +0200

    place test

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,1 +1,68 @@
 #!/usr/bin/python3
+"""
+unit tests for place
+"""
+import unittest
+from models.place import Place
+from models.base_model import BaseModel
+
+
+class TestPlace(unittest.TestCase):
+    """
+    represent place test class
+    """
+    def test_attributes(self):
+        """
+        test attributes
+        """
+        place = Place()
+        self.assertEqual(place.city_id, "")
+        self.assertEqual(place.user_id, "")
+        self.assertEqual(place.name, "")
+        self.assertEqual(place.description, "")
+        self.assertEqual(place.number_rooms, 0)
+        self.assertEqual(place.number_bathrooms, 0)
+        self.assertEqual(place.max_guest, 0)
+        self.assertEqual(place.price_by_night, 0)
+        self.assertEqual(place.latitude, 0.0)
+        self.assertEqual(place.longitude, 0.0)
+        self.assertEqual(place.amenity_ids, [])
+
+    def test_attributes_update(self):
+        """
+        test_attributes_update
+        """
+        place = Place()
+        place.city_id = "123"
+        place.user_id = "456"
+        place.name = "Cozy Cottage"
+        place.description = "A charming getaway"
+        place.number_rooms = 2
+        place.number_bathrooms = 1
+        place.max_guest = 4
+        place.price_by_night = 100
+        place.latitude = 37.7749
+        place.longitude = -122.4194
+        place.amenity_ids = ["wifi", "parking"]
+        self.assertEqual(place.city_id, "123")
+        self.assertEqual(place.user_id, "456")
+        self.assertEqual(place.name, "Cozy Cottage")
+        self.assertEqual(place.description, "A charming getaway")
+        self.assertEqual(place.number_rooms, 2)
+        self.assertEqual(place.number_bathrooms, 1)
+        self.assertEqual(place.max_guest, 4)
+        self.assertEqual(place.price_by_night, 100)
+        self.assertEqual(place.latitude, 37.7749)
+        self.assertEqual(place.longitude, -122.4194)
+        self.assertEqual(place.amenity_ids, ["wifi", "parking"])
+
+    def test_inheritance(self):
+        """
+        Test Iheritance
+        """
+        place = Place()
+        self.assertTrue(isinstance(place, BaseModel))
+
+
+if __name__ == '__main__':
+    unittest.main()
